0c6b12e002a6a71ee591f7c9a3e3ed1d77d14140
Your Name <you@example.com>
2021-08-31 18:27:47 -0300

    Requisito 8 concluido

--- src/challenges.js
+++ src/challenges.js
@@ -2,8 +2,6 @@
 function compareTrue(boolean1, boolean2) {
   if (boolean1 === true && boolean2 === true) {
     return true;
-  } else {
-    return false;
   }
 }
 
@@ -23,8 +21,9 @@ function splitSentence(string) {
 // Desafio 4
 function concatName(array) {
   let firstIten = array[0];
-  let lastIten = array[array.length - 1 ];
-  return lastIten + ', ' + firstIten;
+  let lastIten = array[array.length - 1];
+  let concatenation = lastIten + ', ' + firstIten;
+  return concatenation;
 }
 
 // Desafio 5
@@ -32,17 +31,16 @@ function footballPoints(wins, ties) {
   // wins 3 ties 1
   let pontos;
   if (wins > 1 || ties > 1) {
-    pontos = wins * 3 + ties * 1;    
-  } else { 
-    pontos = 0 
-  }
+    pontos = wins * 3 + ties * 1;
+  } else {
+    pontos = 0;
+   }
   return pontos;
 }
-console.log(footballPoints(0,0))
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount () {
+  // seu código aqui 
 }
 
 // Desafio 7
@@ -51,8 +49,23 @@ function catAndMouse() {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let newArray = [];
+
+  for (let index = 0; index <= array.length-1; index +=1){
+
+    if (array[index] % 3 === 0 && array[index] % 5 === 0) {
+      newArray.push("fizzBuzz");
+
+    } else if (array[index] % 5 === 0) {
+      newArray.push ("buzz");
+
+    } else if (array[index] % 3 === 0) {
+      newArray.push("fizz");
+
+    } else { newArray.push("bug!")};
+  }
+  return newArray;
 }
 
 // Desafio 9
