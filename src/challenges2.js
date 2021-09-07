5b13bde3dce14ac9527197285a2eee46a9a32f56
Your Name <you@example.com>
2021-09-07 11:04:21 -0300

    requisito 13 concluido

--- src/challenges2.js
+++ src/challenges2.js
@@ -11,8 +11,8 @@ function techList(arrayTech, name) {
    *     retornar essa lista ordenada como saida da função
    *     se o array estiver vazio, retornar "vazio"
    */
-  
-   if (arrayTech < 1) {
+
+  if (arrayTech < 1) {
     return 'Vazio!';
   } else {
     let arrayOrder = arrayTech.sort();
@@ -23,7 +23,7 @@ function techList(arrayTech, name) {
       }
       topTech.push(techTech);
     }
-  return topTech;
+    return topTech;
   }
 }
 
@@ -38,10 +38,50 @@ function triangleCheck() {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
-}
+/**
+ * Descobrir quanto foi bebido - match
+ *    criou ujm array com copos
+ *        somar os valores do array - for
+ *              valor do array = copos de agua
+ */
+function hydrate(_string) {
+  let numberDrink = _string.match(/\d+/g);
+  // .match(/\d+/g) - pequeno imprevisto, isso retornou strings entao a soma passou a ser 1+5 = 15
+  // for = if/else para voltar a ser numeral considerando o criterio <9
+  let number = [];
+  for (let i = 0; i < numberDrink.length; i += 1) {
+    if (numberDrink[i] == 1) {
+      number.push(1);
+    } else if (numberDrink[i] == 2){
+      number.push(2);
+    } else if (numberDrink[i] == 3){
+      number.push(3);
+    } else if (numberDrink[i] == 4){
+      number.push(4);
+    } else if (numberDrink[i] == 5){
+      number.push(5);
+    } else if (numberDrink[i] == 6){
+      number.push(6);
+    } else if (numberDrink[i] == 7){
+      number.push(7);
+    } else if (numberDrink[i] == 8){
+      number.push(8);
+    } else if (numberDrink[i] == 9){
+      number.push(9);
+    }
+  }
 
+  let sumArray = 0;
+  for (let index = 0; index < number.length; index += 1) {
+    sumArray += number[index];
+  }
+
+  if (sumArray > 1) {
+    return sumArray + ' copos de água';
+  } else {
+    return sumArray + ' copo de água';
+  }
+}
 module.exports = {
   generatePhoneNumber,
   techList,
